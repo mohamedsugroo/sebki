7ce93da44d66a537529fa52f0f2c4bd2f7916431
mohamedsugroo <mohamedsugroo@gmail.com>
2018-05-01 21:45:32 +0100

    navbar

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,28 +1,21 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
-  # GET /companies
-  # GET /companies.json
   def index
     @companies = Company.all
   end
 
-  # GET /companies/1
-  # GET /companies/1.json
+
   def show
   end
 
-  # GET /companies/new
   def new
     @company = Company.new
   end
 
-  # GET /companies/1/edit
   def edit
   end
 
-  # POST /companies
-  # POST /companies.json
   def create
     @company = Company.new(company_params)
 
@@ -37,8 +30,6 @@ class CompaniesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /companies/1
-  # PATCH/PUT /companies/1.json
   def update
     respond_to do |format|
       if @company.update(company_params)
@@ -51,8 +42,6 @@ class CompaniesController < ApplicationController
     end
   end
 
-  # DELETE /companies/1
-  # DELETE /companies/1.json
   def destroy
     @company.destroy
     respond_to do |format|
@@ -62,12 +51,10 @@ class CompaniesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_company
       @company = Company.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
       params.require(:company).permit(:company_number, :name, :bio, :phone_number, :email, :logo)
     end
